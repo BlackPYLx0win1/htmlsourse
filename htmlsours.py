95ce676092fd46ef492b4584f452c2eb392a5bf0
BlackPYLx0win1 <40028596+BlackPYLx0win1@users.noreply.github.com>
2018-06-08 03:19:12 +0430

    Create htmlsours.py

---
+++ htmlsours.py
@@ -0,0 +1,10 @@
+_authur_ = "BlackPY"
+print("*******************")
+print("*******************")
+print("*****  BlackPY   *****")
+print(" My channel in Telegram : @Termux_Tools")
+print("*******************")
+import requests 
+link = input("Enter sitename:")
+req = requests.get (link)
+print(req.text)
